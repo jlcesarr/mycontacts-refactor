e3713dd30ab86b17ddb5f140837887acad68e695
Julio Cesar <jcesarpereira2021@gmail.com>
2022-11-12 22:22:46 -0300

    refactor(database): add optional param to query function

--- src/database/index.ts
+++ src/database/index.ts
@@ -12,7 +12,7 @@ client.connect()
   .then(() => console.log('DB has connected!'))
   .catch(() => console.log('DB Connection was failed!'))
 
-export async function execQuery<T = any> (query: string): Promise<T[]> {
-  const { rows } = await client.query(query)
+export async function execQuery<T = any> (query: string, values?: any[]): Promise<T[]> {
+  const { rows } = await client.query(query, values)
   return rows
 }
