0108785bf99a4f85ac8bcae6166ce29da89eff5f
Julio Cesar <jcesarpereira2021@gmail.com>
2022-11-12 21:41:57 -0300

    feat(database): create client connection

---
+++ src/database/index.ts
@@ -0,0 +1,18 @@
+import { Client } from 'pg'
+
+const client = new Client({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  port: 5432,
+  database: 'mycontacts'
+})
+
+client.connect()
+  .then(() => console.log('DB has connected!'))
+  .catch(() => console.log('DB Connection was failed!'))
+
+export async function execQuery<T = any> (query: string): Promise<T[]> {
+  const { rows } = await client.query(query)
+  return rows
+}
