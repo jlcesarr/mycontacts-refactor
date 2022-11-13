d6fc01e155c243de4d414de0554931652b2621a4
Julio Cesar <jcesarpereira2021@gmail.com>
2022-11-13 10:47:46 -0300

    feat(database): create categories table

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -4,6 +4,11 @@ CREATE DATABASE mycontacts;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TABLE IF NOT EXISTS categories(
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    name VARCHAR NOT NULL UNIQUE
+);
+
 CREATE TABLE IF NOT EXISTS contacts(
     id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
     name VARCHAR NOT NULL,
